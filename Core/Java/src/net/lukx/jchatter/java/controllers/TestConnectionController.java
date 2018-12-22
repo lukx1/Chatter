b47ff77da5dd35b8870753417967965dc8b9233e
lukx1 <lukx1@lukx.net>
2018-12-23 00:26:52 +0100

    Connected Java chatter to shared library Made an example get all users page

---
+++ Core/Java/src/net/lukx/jchatter/java/controllers/TestConnectionController.java
@@ -0,0 +1,68 @@
+package net.lukx.jchatter.java.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import net.lukx.jchatter.lib.comms.Communicator;
+import net.lukx.jchatter.lib.models.User;
+import net.lukx.jchatter.lib.repos.UserRepo;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class TestConnectionController {
+
+    public Button TestBtn;
+    public Label Output;
+    public TextField Login;
+    public PasswordField Password;
+    public TextField Uri;
+
+    public TestConnectionController(){
+
+    }
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+
+    }
+
+    private String UsersToString(User[] users){
+        StringBuilder b = new StringBuilder();
+        for (User user : users) {
+            b.append(user.id).append("\n");
+            b.append(user.status).append("\n");
+            b.append(user.email).append("\n");
+            b.append(user.firstName).append("\n");
+            b.append(user.secondName).append("\n");
+            b.append(user.login).append("\n");
+            b.append(user.password).append("\n");
+            b.append(user.pictureUrl).append("\n");
+            b.append(user.dateRegistered).append("\n");
+            b.append(user.dateLastLogin).append("\n");
+            b.append("---");
+        }
+        return b.toString();
+    }
+
+    public void btnClicked(ActionEvent actionEvent) {
+        Communicator c = new Communicator();
+        try {
+            c.setServerURI(new URI(Uri.getText()));
+            UserRepo userRepo = new UserRepo(c);
+            userRepo.getLoginHeader().setPassword(Password.getText());
+            userRepo.getLoginHeader().setLogin(Login.getText());
+            User[] users = userRepo.getUsers();
+            Output.setText(UsersToString(users));
+        } catch (URISyntaxException | IOException e) {
+            Output.setText(e.getLocalizedMessage());
+        }
+    }
+    
+    /*public void TestBtnClicked(MouseEvent mouseEvent) {
+
+    }*/
+}
