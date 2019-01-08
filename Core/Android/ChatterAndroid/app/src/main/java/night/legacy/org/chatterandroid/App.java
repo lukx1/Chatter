3834c045c34c98d7a8f91389e28bf2f1cfc326c9
Nightraw <ramboplay23@gmail.com>
2019-01-08 23:19:10 +0100

    Funguje Login a Register, Přidán ApiConnector.java

--- Core/Android/ChatterAndroid/app/src/main/java/night/legacy/org/chatterandroid/App.java
+++ Core/Android/ChatterAndroid/app/src/main/java/night/legacy/org/chatterandroid/App.java
@@ -2,15 +2,26 @@ package night.legacy.org.chatterandroid;
 
 import net.lukx.jchatter.lib.models.User;
 
+import java.net.URISyntaxException;
+
 public class App {
-    private static final App ourInstance = new App();
+    private static App ourInstance = null;
+
+    static {
+        try {
+            ourInstance = new App();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static App getInstance() {
         return ourInstance;
     }
 
-    private App() {
+    private App() throws URISyntaxException {
     }
 
     public User LoggedUser;
+    public ApiConnector Connector = new ApiConnector();
 }
